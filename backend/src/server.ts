f4f25266cf31c00eb9d13142a7e65da757eb1a24
Puyan Wei <puyanwei@gmail.com>
2025-03-10 08:37:35 +0000

    backend build fixes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -42,5 +42,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = parseInt(process.env.PORT || '5000', 10);
+server.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
