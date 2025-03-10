1199d381aa0d16a0d8eb0ceef40d46f9f6c864f6
Puyan Wei <puyanwei@gmail.com>
2025-03-10 18:50:30 +0000

    frontend build fixes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
+  httpAgentOptions: {
+    keepAlive: true,
+  },
   webpack: (config) => {
     config.externals = [...(config.externals || []), { bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate" }];
     return config;
