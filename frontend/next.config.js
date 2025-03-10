40a7425131219dad725019e6b1d80e58d48a8574
Puyan Wei <puyanwei@gmail.com>
2025-03-10 19:26:28 +0000

    frontend build fixes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   webpack: (config) => {
     config.externals = [...(config.externals || []), { bufferutil: "bufferutil", "utf-8-validate": "utf-8-validate" }];
