9a056613e1d2e494c1b0299992018ee14c5a2d54
Puyan Wei <puyanwei@gmail.com>
2025-05-25 23:44:07 +0100

    more logs

--- frontend/src/components/game-modules/tictactoe/TicTacToe.tsx
+++ frontend/src/components/game-modules/tictactoe/TicTacToe.tsx
@@ -19,6 +19,9 @@ export function TicTacToe() {
     isConnected,
   } = useTicTacToe();
 
+  console.log("gameStarted", gameStarted);
+  console.log("gameFinished", gameFinished);
+
   const [inputRoomId, setInputRoomId] = useState("");
 
   const handleMove = (index: number) => {
